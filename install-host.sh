11c07903ef9fe0cf908404fb772e568aa89710d7
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2013-08-07 02:42:24 +0000

    move rsyslog and fix installhost

--- install-host.sh
+++ install-host.sh
@@ -8,8 +8,7 @@ then
     exit 1
 fi
 
-CURDIR="$( cd `dirname "${BASH_SOURCE[0]}` && pwd )"
-source $CURDIR/functions.sh
+DIR=/shared
 
 # start
 apt-get update
@@ -17,14 +16,17 @@ apt-get -y install software-properties-common byobu zsh git htop mc ufw
 
 # LXC
 add-apt-repository ppa:ubuntu-lxc/daily
-apt-get -y install lxc
+apt-get install lxc
 
 mkdir -p $DIR
 
-# dotfiles & others
 git clone https://github.com/niieani/lxc-shared.git ${DIR}
 (cd $DIR; git submodule init && git submodule update && git submodule status)
 
-source $CURDIR/create-host-links.sh
+#CURDIR="$( cd `dirname "${BASH_SOURCE[0]}` && pwd )"
+#source $CURDIR/functions.sh
+
+# dotfiles & others
+source $DIR/create-host-links.sh
 
 chsh -s /bin/zsh
