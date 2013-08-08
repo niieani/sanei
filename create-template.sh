a30f87540069089b5a7f14e34239f41c42c50fba
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2013-08-08 12:29:36 +0200

    support different tpl names

--- create-template.sh
+++ create-template.sh
@@ -5,14 +5,22 @@ then
     exit 1
 fi
 
+if [ -z $1 ]; then
+        echo "No name given"
+        exit 1
+fi
+
 CURDIR="$( cd `dirname "${BASH_SOURCE[0]}"` && pwd )"
+
 source $CURDIR/functions.sh
 
-if [[ ! -e /lxc ]]; then ln -v -s /var/lib/lxc /lxc; fi
-lxc-create -t ubuntu -n template
-echo "/shared shared none defaults,bind 0 0" >> /lxc/template/fstab
+TEMPLATE_NAME=$1
 
-TEMPLATE_ROOT=/lxc/template/rootfs
+if [[ ! -e /lxc ]]; then ln -v -s /var/lib/lxc /lxc; fi
+lxc-create -t ubuntu -n $TEMPLATE_NAME
+echo "/shared shared none defaults,bind 0 0" >> /lxc/$TEMPLATE_NAME/fstab
+
+TEMPLATE_ROOT=/lxc/$TEMPLATE_NAME/rootfs
 BACKUP_DIR=${TEMPLATE_ROOT}/root/.backups
 
 # /shared in containers
