65a5fe801f8102be7219c5e1f0885fc80a703459
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2013-08-26 00:20:05 +0000

    phpmyadmin

---
+++ add-phpmyadmin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CURDIR="$( cd `dirname "${BASH_SOURCE[0]}"` && pwd )"
+source $CURDIR/functions.sh
+askbreak "Really?"
+
+add-apt-repository -y ppa:nijel/phpmyadmin
+#apt-get update
+apt-get install -y mariadb-client
+source $CURDIR/add-www+mysql.sh
+apt-get install -y phpmyadmin
+
+#apt-get install -y nginx-light php5-fpm phpmyadmin php-apc lsof
+chown -R www-data.www-data /usr/share/phpmyadmin
+
+#apt-get install -y nginx-light php5-fpm php5-gd php5-curl php-pear php-apc lsof
+#ufw allow "nginx full"
+
+set_installed phpmyadmin
