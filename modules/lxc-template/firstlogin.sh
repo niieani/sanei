4df0f8c83ac5b09a5c57aa61d32307f8b267715b
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2013-09-01 12:47:48 +0200

    firstlogin

--- modules/lxc-template/firstlogin.sh
+++ modules/lxc-template/firstlogin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # locking
- lockdir=/tmp/first-run.lock
+ lockdir=/tmp/firstlogin.lock
  if mkdir "$lockdir"
  then    # directory did not exist, but was created successfully
      echo >&2 "successfully acquired lock: $lockdir"
@@ -11,10 +11,11 @@
      rm ~/.bash_profile
      apt-get -y install zsh htop mc software-properties-common ufw wget dialog
      apt-get --purge -y remove openssh-server
+     ln -s $SCRIPT_DIR/sanei /usr/bin/sanei
      ufw allow lxc-net
      ufw enable
      chsh -s /bin/zsh
-     rmdir /tmp/first-run.lock
+     rmdir "$lockdir"
      exit
  else
      #echo >&2 "cannot acquire lock, giving up on $lockdir"
