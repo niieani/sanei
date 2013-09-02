d7605b892e8128c426eb84d227156e49d4639940
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2013-09-02 18:09:23 +0200

    fixes for obnam

--- modules/obnam/schedule.sh
+++ modules/obnam/schedule.sh
@@ -6,7 +6,10 @@ fi
 
 non_default_setting_needed OBNAM_REPOSITORY
 store_memory_config CONTAINER_NAME "$1"
-sanei module gpg-key generate $CONTAINER_NAME
+if ! sanei module gpg-key generate obnam_$CONTAINER_NAME; then
+	error "Cannot continue."
+	exit 1;
+fi
 
-# store_memory_config CONTAINER_DIR "/var/lib/lxc/$CONTAINER_NAME"
+store_memory_config GPG_KEY_ID $(cat "$GPG_KEY_ID_PATH/obnam_$CONTAINER_NAME")
 fill_template $MODULE_DIR/templates/lxc-container.conf /opt/obnam/$CONTAINER_NAME.conf
