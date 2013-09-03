33c30f00875af094833293676e06f8172dc6da66
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2013-09-03 19:27:23 +0200

    obnam module developement

--- modules/obnam/install.sh
+++ modules/obnam/install.sh
@@ -1,3 +1,17 @@
+non_default_setting_needed OBNAM_REPOSITORY
+sanei_install_dependencies ssh-key
+
 apt_install obnam "ppa:chris-bigballofwax/obnam-ppa"
 mkdir -p /opt/obnam
+
+get_obnam_repository_details
+
+# TODO:
+if [[ $OBNAM_REPOSITORY == sftp\:* ]]; then
+	echo "Enter your Backup User SSH password:"
+    # repository_server=$(echo "$OBNAM_REPOSITORY" | cut -c "8-")
+	ssh-copy-id "-p$repository_server_port $repository_server"
+	ssh $repository_server -p$repository_server_port "mkdir -p $OBNAM_REPOSITORY/$LOCAL_HOSTNAME"
+fi
+
 set_installed obnam
