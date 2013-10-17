af316be8cf6a945e085081b3009a8a46eb379310
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2013-10-18 01:29:47 +0200

    mysql local

--- modules/openfire/install.sh
+++ modules/openfire/install.sh
@@ -2,3 +2,5 @@ cd /tmp
 wget -O openfire_3.8.2_all.deb http://www.igniterealtime.org/downloadServlet?filename=openfire/openfire_3.8.2_all.deb
 dpkg -i openfire_3.8.2_all.deb
 sed -i "s/6-sun/7-oracle/g" "/etc/init.d/openfire"
+
+ufw allow Openfire
