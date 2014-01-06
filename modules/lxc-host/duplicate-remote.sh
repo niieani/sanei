cc4a6148b5956a701a82d03a2540a17a802c0241
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2014-01-06 22:31:21 +0100

    temporary setting

--- modules/lxc-host/duplicate-remote.sh
+++ modules/lxc-host/duplicate-remote.sh
@@ -6,5 +6,5 @@ if [[ ! $DONOTSTOP ]]; then
 	ssh "${server}" -p$port "lxc-stop -n container"
 fi
 
-rsync -Havz --ignore-existing --progress --exclude=cache/* --exclude=tmp/* --exclude=*backup* --rsh="ssh -p$port" ${server}:/lxc/$container "/lxc/$container"
+rsync -Havz --ignore-existing --progress --exclude=cache/* --exclude=tmp/* --exclude=*backup* --exclude=*web/htdocs* --rsh="ssh -p$port" ${server}:/lxc/$container "/lxc/$container"
 # preserves symlinks, archive mode (for copying system), verbose, compressed 
