1e19844da3ff65bb5a586f349f2bfc67db5acb0b
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2014-10-07 14:22:04 +0200

    remove lxc daily ppa & tmux ppa

--- modules/lxc-host/install.sh
+++ modules/lxc-host/install.sh
@@ -1,13 +1,13 @@
 sanei_resolve_dependencies lxc-common dotfiles
 
 # LXC
-apt_install "lxc" "ppa:ubuntu-lxc/daily"
+#apt_install "lxc" "ppa:ubuntu-lxc/daily"
 
 ln -s /var/lib/lxc /lxc
 
 # byobu & tmux # TODO: move
 apt_install "byobu" "ppa:byobu/ppa"
-apt_install "tmux" "ppa:chris-reeves/tmux"
+#apt_install "tmux" "ppa:chris-reeves/tmux"
 
 set_installed lxc-host
 
