965eb95658ed80ceb0e64449f1799e662f2a4bcf
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2014-10-15 19:56:32 +0200

    fix testmail

--- modules/relaymail/test.sh
+++ modules/relaymail/test.sh
@@ -1,10 +1,10 @@
-echo "To: $RELAYMAIL_TEST_EMAIL" > /tmp/mail.test
-cat <<EOM >> /tmp/mail.test
+cat <<- EOF > /tmp/mail.test
+To: $RELAYMAIL_TEST_EMAIL
 Subject: Put a subject here
 From: test
 
 Of cause, here's the place to put the body
-EOM
+EOF
 
 sendmail -vt < /tmp/mail.test
 rm /tmp/mail.test
