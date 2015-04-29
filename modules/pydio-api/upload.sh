a7c2b20061211871b13017c36672451b1fcba661
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2015-04-29 21:23:02 +0000

    upload progress bar

--- modules/pydio-api/upload.sh
+++ modules/pydio-api/upload.sh
@@ -8,4 +8,5 @@ curl \
     -u "$PYDIO_LOGIN:$PYDIO_PASSWORD" \
     --upload-file "$file" \
     --header "X-File-Name:$(basename '$file')" \
-    "$PYDIO_HOST/api/${PYDIO_REPOSITORY}/upload/input_stream${PYDIO_TARGET_DIR}?auto_rename=${PYDIO_AUTORENAME}"
+    "$PYDIO_HOST/api/${PYDIO_REPOSITORY}/upload/input_stream${PYDIO_TARGET_DIR}?auto_rename=${PYDIO_AUTORENAME}" \
+    > /dev/null # redirect required to show a progress bar
