04dacda0360d4b3fc3d56facbae01a2667da9e1c
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2015-04-29 21:30:09 +0000

    fix filename

--- modules/pydio-api/upload.sh
+++ modules/pydio-api/upload.sh
@@ -7,6 +7,6 @@ PYDIO_AUTORENAME="false";
 curl \
     -u "$PYDIO_LOGIN:$PYDIO_PASSWORD" \
     --upload-file "$file" \
-    --header "X-File-Name:$(basename '$file')" \
+    --header "X-File-Name:$(basename "$file")" \
     "$PYDIO_HOST/api/${PYDIO_REPOSITORY}/upload/input_stream${PYDIO_TARGET_DIR}?auto_rename=${PYDIO_AUTORENAME}" \
     > /dev/null # redirect required to show a progress bar
